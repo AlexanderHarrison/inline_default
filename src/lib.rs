f516682854585cd62452869d6ec1b0479dcb97ca
Thomas ten Cate <ttencate@gmail.com>
2023-01-02 14:35:44 +0100

    Add support for doc comments and other metadata on fields

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@ macro_rules! inline_default {
         $(#[$s_at:meta])* // attributes
         $s_v:vis struct $s_nm:ident //struct name, vis
         $(< $($s_lt:lifetime,)* $( $s_gen:ident $(: $s_tr:ident)? ),* $(,)? >)? // generics
-        { $($f_v:vis $f_nm:ident : $f_ty:ty $(= $def:expr)? ),* $(,)? } // definition
+        { $($(#[$f_at:meta])* $f_v:vis $f_nm:ident : $f_ty:ty $(= $def:expr)? ),* $(,)? } // definition
     ) => {
         $(#[$s_at])*
         $s_v struct $s_nm $(< $($s_lt ,)* $($s_gen $(: $s_tr)? ),* >)? {
-            $( $f_v $f_nm : $f_ty ),*
+            $( $(#[$f_at])* $f_v $f_nm : $f_ty ),*
         }
 
         macro_rules! __default_expand {
